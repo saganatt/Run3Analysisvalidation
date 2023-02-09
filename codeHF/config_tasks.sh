5a74b577b19efe2ad49705d38dcf3fa8e5c7f058
Maja Kabus <mkabus@cern.ch>
2023-02-09 16:00:02 +0100

    Add flag to convert

--- codeHF/config_tasks.sh
+++ codeHF/config_tasks.sh
@@ -15,7 +15,7 @@
 
 # Steps
 DOCLEAN=1           # Delete created files (before and after running tasks).
-DOCONVERT=0         # Convert AliESDs.root to AO2D.root.
+DOCONVERT=1         # Convert AliESDs.root to AO2D.root.
 DOALI=0             # Run AliPhysics tasks.
 DOO2=1              # Run O2 tasks.
 DOPOSTPROCESS=0     # Run output postprocessing. (Comparison plots. Requires DOALI=1 and/or DOO2=1)
