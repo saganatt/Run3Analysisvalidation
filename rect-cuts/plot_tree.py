3fe4096a11f33ba325c346f035f4c024419c4054
Maja Kabus <mkabus@cern.ch>
2023-06-06 17:16:09 +0200

    Start of the macro to plot from Luigi's trees

---
+++ rect-cuts/plot_tree.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+"""
+file: plot_tree.py
+brief: Plotting most important distributions for rectangular cuts
+usage: ./plot_tree.py AnalysisResults_tree_signal.root AnalysisResults_tree_bkg.root
+author: Maja Kabus <mkabus@cern.ch>, CERN / Warsaw University of Technology
+"""
+
+import argparse
+from array import array
+
+# pylint: disable=import-error,no-name-in-module
+from ROOT import (
+    TCanvas,
+    TFile,
+    TLegend,
+    gROOT,
+    gStyle,
+)
+
+
+def save_canvas(canvas, title):
+    """
+    Save canvas in png, pdf, root.
+    """
+    format_list = [".png", ".pdf", ".root"]
+    for file_format in format_list:
+        canvas.SaveAs(title + file_format)
+
+
+def prepare_canvas(var):
+    """
+    Initialize canvas.
+    """
+    canv = TCanvas(f"c_{var}", var)
+    canv.SetCanvasSize(800, 600)
+    canv.cd()
+    canv.SetGridy()
+    canv.SetGridx()
+    return canv
+
+
+def get_proj(hist, var, var_range, var_file, pt_range, sig_or_bkg):
+    """
+    Get the variable and pt from the tree.
+    """
+    var_val = array('f', [ 0. ])
+    t.SetBranchAddress(f'f{var}', var_val)
+
+
+
+    bin1 = hist.GetYaxis().FindBin(pt_range[0])
+    bin2 = hist.GetYaxis().FindBin(pt_range[1])
+    h_proj = hist.ProjectionX(f"hproj_{sig_or_bkg}_{var_file}_"
+                              f"pt_{pt_range[0]}-{pt_range[1]}", bin1, bin2)
+    h_proj.Scale(1. / h_proj.Integral()) # probability distribution, sum of content = 1.0
+
+    h_proj.SetTitle(f"Normalized {var} for {pt_range[0]} #leq #it{{p}}_{{T}} < {pt_range[1]}")
+    h_proj.GetXaxis().SetRangeUser(var_range[0], var_range[1])
+    if sig_or_bkg == "sig":
+        h_proj.SetLineColor(1)
+    else:
+        h_proj.SetLineColor(2)
+    #hist.GetYaxis().CenterTitle()
+    #hist.GetXaxis().CenterTitle()
+    #hist.GetXaxis().SetNoExponent()
+    gStyle.SetOptStat(0)
+    gStyle.SetFrameLineWidth(2)
+    #gStyle.SetTitleSize(0.045, "x")
+    #gStyle.SetTitleSize(0.045, "y")
+    #gStyle.SetMarkerSize(1)
+    #gStyle.SetLabelOffset(0.015, "x")
+    #gStyle.SetLabelOffset(0.02, "y")
+    #gStyle.SetTitleOffset(1.1, "x")
+    #gStyle.SetTitleOffset(1.0, "y")
+    return h_proj
+
+
+def plot(hist_sig, hist_bkg, var, var_range, pt_range):
+    """
+    Plot a 1D histogram for a given variable and pT range.
+    """
+    canv = prepare_canvas(f"{var}_pt_{pt_range[0]}-{pt_range[1]}")
+    var_file = var.replace(" ", "-")
+
+    sig_h = get_proj(hist_sig, var, var_range, var_file, pt_range, "sig")
+    bkg_h = get_proj(hist_bkg, var, var_range, var_file, pt_range, "bkg")
+    sig_h.Draw("hist")
+    bkg_h.Draw("hist;same")
+    legend = TLegend(0.50, 0.72, 0.70, 0.90)
+    legend.AddEntry(sig_h, "signal", "L")
+    legend.AddEntry(bkg_h, "background", "L")
+    legend.Draw()
+
+    save_canvas(canv, f"{var_file}_pt_{pt_range[0]}-{pt_range[1]}")
+
+
+def main():
+    """
+    Main function.
+    """
+    gROOT.SetBatch(True)
+    parser = argparse.ArgumentParser(description="Arguments to pass")
+    parser.add_argument("sig_input_file", help="input signal tree AnalysisResults_tree.root file")
+    parser.add_argument("bkg_input_file", help="input background tree AnalysisResults_tree.root file")
+
+    args = parser.parse_args()
+
+    settings = { "var_list": ["decay length", "decay length XY", "CPA", "CPA XY"],
+                 "leaf_list": ["fDecayLength", "fDecayLengthXY", "fCPA", "fCPAXY"],
+                 "var_ranges": [[-0.05, 0.45], [-0.05, 0.45], [0.45, 1.05], [0.45, 1.05]],
+                 "pt_ranges": [0, 1, 2, 4, 6, 8, 12, 24]
+                }
+
+    infile_sig = TFile(args.sig_input_file)
+    infile_bkg = TFile(args.bkg_input_file)
+
+    tree_name = "O2hfcand3pfull"
+    sig_trees = []
+    for key in infile_sig.GetListOfKeys():
+        if key.GetName(): # is the dataframe directory
+            sig_dirs.append(key.Get(tree_name))
+
+    # Prepare canvas and histograms for each var
+
+    hists = { }
+
+    for tree in sig_trees:
+        pt = array("f", [ 0. ])
+        tree.SetBranchAddress("fPt", pt)
+        var_leaves = { var: array("f", [0.]) for var in settings["var_list"] }
+        for var, leaf in zip(settings["var_list"], settings["leaf_list"]):
+            tree.SetBranchAddress(leaf, var_leaves[var])
+        for i in tree.GetEntries():
+            tree.GetEntry(i)
+            for var, leaf in var_leaves:
+                hists[var].Fill(leaf.GetValue())
+    
+    # plot and save canvas
+
+    for histname, var, var_range in \
+            zip(settings["hist_list"], settings["var_list"], settings["var_ranges"]):
+        print(f'Getting histogram: {settings["hist_path"]}/{histname}')
+        hist_sig = infile_sig.Get(f'{settings["hist_path"]}/{histname}')
+        hist_bkg = infile_bkg.Get(f'{settings["hist_path"]}/{histname}')
+        for ind in range(len(settings["pt_ranges"]) - 1):
+            pt_range = (settings["pt_ranges"][ind], settings["pt_ranges"][ind + 1])
+            plot(hist_sig, hist_bkg, var, var_range, pt_range)
+
+
+if __name__ == "__main__":
+    main()
