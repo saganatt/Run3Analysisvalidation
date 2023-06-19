9e89d74439eaf2fb875520502a921b19f58645a1
Maja Kabus <mkabus@cern.ch>
2023-06-19 11:18:35 +0200

    Plot pt distributions and impact parameter

--- rect-cuts/plot_trees.py
+++ rect-cuts/plot_trees.py
@@ -13,8 +13,8 @@ from array import array
 from ROOT import (
     TCanvas,
     TFile,
-    TTree,
-    TList,
+    #TTree,
+    #TList,
     TLegend,
     TH1F,
     gROOT,
@@ -39,28 +39,36 @@ def prepare_hists(settings):
     hists_bkg = { var: [] for var in settings["var_list"] }
     gStyle.SetOptStat(0)
     gStyle.SetFrameLineWidth(2)
-    for i in range(len(settings["pt_ranges"]) - 1):
-        for var, varu, var_range in zip(settings["var_list"], settings["var_list_u"],
-                                        settings["var_ranges"]):
-            histname = f'{varu}_pt_{settings["pt_ranges"][i]}-{settings["pt_ranges"][i + 1]}'
-            hist_title = f'Normalized {var} for {settings["pt_ranges"][i]} #leq #it{{p}}_{{T}} <' \
-                         f'{settings["pt_ranges"][i + 1]}'
-            hist_sig = TH1F(f"h_sig_{histname}", hist_title, *var_range)
-            hist_bkg = TH1F(f"h_bkg_{histname}", hist_title, *var_range)
-            hist_sig.SetLineColor(1)
-            hist_bkg.SetLineColor(2)
-            hists_sig[var].append(hist_sig)
-            hists_bkg[var].append(hist_bkg)
+
+    def prepare_hist_single(histname, hist_title):
+        hist_sig = TH1F(f"h_sig_{histname}", f"{hist_title}", *var_range)
+        hist_bkg = TH1F(f"h_bkg_{histname}", f"{hist_title}", *var_range)
+        hist_sig.SetLineColor(1)
+        hist_bkg.SetLineColor(2)
+        hists_sig[var].append(hist_sig)
+        hists_bkg[var].append(hist_bkg)
+
+    for var, varu, var_range in zip(settings["var_list"], settings["var_list_u"],
+                                    settings["var_ranges"]):
+        if "pt_ranges" in settings:
+            for i in range(len(settings["pt_ranges"]) - 1):
+                histname = f'{varu}_pt_{settings["pt_ranges"][i]}-{settings["pt_ranges"][i + 1]}'
+                hist_title = f'Normalized {var} for {settings["pt_ranges"][i]}' \
+                             f' #leq #it{{p}}_{{T}} < {settings["pt_ranges"][i + 1]}'
+                prepare_hist_single(histname, hist_title)
+        else:
+            prepare_hist_single(varu, f"Normalized {var}")
+
     return hists_sig, hists_bkg
 
 
-def fill_hists(settings, infile, hists, sig_or_bkg, outfile): # pylint: disable=too-many-locals
+def fill_hists(settings, settings_pt, infile, hists, hists_pt, sig_or_bkg, outfile): # pylint: disable=too-many-locals, too-many-arguments, too-many-branches
     """
     Fill histograms from trees.
     """
     tree_name = "O2hfcand3pfull"
     #tree_list = TList()
-    count = 0
+    #count = 0
     for key in infile.GetListOfKeys(): # pylint: disable=too-many-nested-blocks
         key_name = key.GetName()
         if key_name.startswith("DF_"): # is the dataframe directory
@@ -73,8 +81,11 @@ def fill_hists(settings, infile, hists, sig_or_bkg, outfile): # pylint: disable=
             mc_flag = array("b", [ 0 ])
             tree.SetBranchAddress("fMCflag", mc_flag)
             var_leaves = { var: array("f", [0.]) for var in settings["var_list"] }
+            var_leaves_pt = { var: array("f", [0.]) for var in settings_pt["var_list"] }
             for var, leaf in zip(settings["var_list"], settings["leaf_list"]):
                 tree.SetBranchAddress(leaf, var_leaves[var])
+            for var, leaf in zip(settings_pt["var_list"], settings_pt["leaf_list"]):
+                tree.SetBranchAddress(leaf, var_leaves_pt[var])
             for i in range(tree.GetEntries()):
                 tree.GetEntry(i)
                 # mc_flag == 1 << DecayType::LcToPKPi, decay type == 1
@@ -85,6 +96,8 @@ def fill_hists(settings, infile, hists, sig_or_bkg, outfile): # pylint: disable=
                             for var in var_leaves:
                                 hists[var][j].Fill(var_leaves[var][0])
                             break
+                    for var in var_leaves_pt:
+                        hists_pt[var][0].Fill(var_leaves_pt[var][0])
             #if count == 5:
             #    break
     #tree_count = tree_list.GetEntries()
@@ -108,6 +121,8 @@ def fill_hists(settings, infile, hists, sig_or_bkg, outfile): # pylint: disable=
             #print(f"Plotting {tree_line}")
             #out_tree.Draw(tree_line, cond(i))
             outfile.WriteObject(hists[var][i], hists[var][i].GetName())
+    for var, leaf in zip(settings_pt["var_list"], settings_pt["leaf_list"]):
+        outfile.WriteObject(hists_pt[var][0], hists_pt[var][0].GetName())
 
 
 def plot_single(h_sig, h_bkg, filename):
@@ -165,6 +180,28 @@ def plot_file(outfile, settings):
             plot_single(h_sig, h_bkg, histname)
 
 
+def plot_pt(hists_sig, hists_bkg, settings):
+    """
+    Plot momenta distributions.
+    """
+    for var, varu in zip(settings["var_list"], settings["var_list_u"]):
+        h_sig = hists_sig[var][0]
+        h_bkg = hists_bkg[var][0]
+        filename = varu
+        plot_single(h_sig, h_bkg, filename)
+
+
+def plot_pt_file(outfile, settings):
+    """
+    Plot momenta distributions from file.
+    """
+    for varu in settings["var_list_u"]:
+        histname = varu
+        h_sig = outfile.Get(f"h_sig_{histname}")
+        h_bkg = outfile.Get(f"h_bkg_{histname}")
+        plot_single(h_sig, h_bkg, histname)
+
+
 def main():
     """
     Main function.
@@ -180,33 +217,45 @@ def main():
     args = parser.parse_args()
 
     settings = { "var_list": ["decay length", "decay length XY", "CPA", "CPA XY",
-                              "Chi2PCA", "mass"],
+                              "Chi2PCA", "mass",
+                              "impact parameter 0", "impact parameter 1", "impact parameter 2"],
                  "var_list_u": ["decay_length", "decay_length_XY", "CPA", "CPA_XY",
-                                "Chi2PCA", "mass"],
+                                "Chi2PCA", "mass",
+                                "impact_parameter_0", "impact_parameter_1", "impact_parameter_2"],
                  "leaf_list": ["fDecayLength", "fDecayLengthXY", "fCPA", "fCPAXY",
-                               "fChi2PCA", "fM"],
+                               "fChi2PCA", "fM",
+                               "fImpactParameter0", "fImpactParameter1", "fImpactParameter2"],
                  "var_ranges": [[100, 0.0, 0.1], [100, 0.0, 0.1], [100, 0.9, 1.], [100, 0.9, 1.],
-                                [200, 0., 0.01], [600, 1.98, 2.58]],
+                                [200, 0., 0.01], [600, 1.98, 2.58],
+                                [100, -0.1, 0.1], [100, -0.1, 0.1], [100, -0.1, 0.1]],
                  "pt_ranges": [0, 1, 2, 4, 6, 8, 12, 24]
                 }
+    settings_pt = { "var_list": ["pt", "pt prong0", "pt prong1", "pt prong2"],
+                    "var_list_u": ["pt", "pt_prong0", "pt_prong1", "pt_prong2"],
+                    "leaf_list": ["fPt", "fPtProng0", "fPtProng1", "fPtProng2"],
+                    "var_ranges": [[200, 0, 24], [200, 0, 24], [200, 0, 24], [200, 0, 24]],
+                  }
 
     if args.plot_file:
         outfile = TFile(args.outfile)
         plot_file(outfile, settings)
+        plot_pt_file(outfile, settings_pt)
     else:
         infile_sig = TFile(args.sig_input_file)
         infile_bkg = TFile(args.bkg_input_file)
 
         hists_sig, hists_bkg = prepare_hists(settings)
+        hists_sig_pt, hists_bkg_pt = prepare_hists(settings_pt)
 
         print("Filling histos")
         outfile = TFile(args.outfile, "RECREATE")
-        fill_hists(settings, infile_sig, hists_sig, "sig", outfile)
+        fill_hists(settings, settings_pt, infile_sig, hists_sig, hists_sig_pt, "sig", outfile)
         print("Filled histos for signal")
-        fill_hists(settings, infile_bkg, hists_bkg, "bkg", outfile)
+        fill_hists(settings, settings_pt, infile_bkg, hists_bkg, hists_bkg_pt, "bkg", outfile)
         print("Filled histos for background")
 
         plot(hists_sig, hists_bkg, settings)
+        plot_pt(hists_sig_pt, hists_bkg_pt, settings_pt)
 
         infile_sig.Close()
         infile_bkg.Close()
