df11ec01e7c27caa95b3ce6d9a4269af426261a7
Maja Kabus <mkabus@cern.ch>
2023-10-10 15:59:43 +0200

    Refactored plot trees to compare more than 2 inputs and use JSON config. Not tested yet.

--- rect-cuts/plot_trees.py
+++ rect-cuts/plot_trees.py
@@ -9,6 +9,7 @@ author: Maja Kabus <mkabus@cern.ch>, CERN / Warsaw University of Technology
 
 from array import array
 import argparse
+import json
 
 # pylint: disable=import-error,no-name-in-module
 from ROOT import (
@@ -19,6 +20,10 @@ from ROOT import (
     TH2F,
     gROOT,
     gStyle,
+    kBlack,
+    kRed,
+    kBlue,
+    kGreen,
 )
 
 
@@ -31,11 +36,10 @@ def save_canvas(canvas, title):
         canvas.SaveAs(title + file_format)
 
 
-def plot_tree_single(tree, settings_var, hist_title, sig_or_bkg, htype):
+def plot_tree_single(tree, settings_var, hist_title, selection, hcolor, htype):
     """
     Get a single histogram from a tree.
     """
-    hist_color = { "sig": 1, "bkg": 2}
     # mc_flag == 1 << DecayType::LcToPKPi, decay type == 1
     tree_cond = { "sig": "fM > 2.226 && fM < 2.346" if settings_var[1] != "fM" else "", "bkg": "fFlagMc == 0",
                   "sig_mc": "(fFlagMc == 2 || fFlagMc == -2)"}
@@ -43,13 +47,13 @@ def plot_tree_single(tree, settings_var, hist_title, sig_or_bkg, htype):
     histname = f"h_{sig_or_bkg}_{settings_var[0]}"
     if htype == "TH1F":
         hist = TH1F(histname, f"{hist_title}", *settings_var[2])
-        hist.SetLineColor(hist_color[sig_or_bkg])
-        tree.Draw(f"{settings_var[1]}>>{histname}", tree_cond[sig_or_bkg])
+        hist.SetLineColor(hcolor)
+        tree.Draw(f"{settings_var[1]}>>{histname}", tree_cond[selection])
     else:
         hist = TH2F(histname, f"{hist_title}", *settings_var[2], 7,
                     array("d", [0, 1, 2, 4, 6, 8, 12, 24]))
-        hist.SetLineColor(hist_color[sig_or_bkg])
-        tree.Draw(f"fPt:{settings_var[1]}>>{histname}", tree_cond[sig_or_bkg])
+        hist.SetLineColor(hcolor)
+        tree.Draw(f"fPt:{settings_var[1]}>>{histname}", tree_cond[selection])
 
     int_hist = hist.Integral()
     if int_hist != 0.0 and settings_var[1] != "fM":
@@ -70,27 +74,28 @@ def plot_single(hists, c_name):
     canv.SetGridx()
     canv.SetGridy()
 
-    sig_entries = hists["sig"].GetEntries()
-    bkg_entries = hists["bkg"].GetEntries()
-    print(f"Sig histogram for {c_name} counts: {sig_entries} bkg: {bkg_entries}")
-
-    hists["sig"].Draw("hist")
-    hists["bkg"].Draw("hist;same")
-    y_min = min(hists["sig"].GetMinimum(), hists["bkg"].GetMinimum())
-    y_max = max(hists["sig"].GetMaximum(), hists["bkg"].GetMaximum())
-    y_range = y_max - y_min
-    hists["sig"].GetYaxis().SetRangeUser(0.0, y_max + margin / k * y_range)
-    hists["bkg"].GetYaxis().SetRangeUser(0.0, y_max + margin / k * y_range)
-    hists["sig"].GetYaxis().SetTitle("Normalized counts")
-    hists["bkg"].GetYaxis().SetTitle("Normalized counts")
+    counts_str = f"Sig histogram for {c_name} counts:"
+    option = "hist"
+    for hist in hists:
+        entries = hist.GetEntries()
+        counts_str = f"{counts_str} {entries}"
+        hist.Draw(option)
+        option = "hist;same"
+    print(counts_str)
 
     legend = TLegend(0.50, 0.72, 0.70, 0.90)
-    legend.AddEntry(hists["sig"], "LHC22o bkg", "L")
-    legend.AddEntry(hists["bkg"], "LHC23f4b2 bkg", "L")
+
+    y_min = min((hist.GetMinimum() for hist in hists))
+    y_max = min((hist.GetMaximum() for hist in hists))
+    y_range = y_max - y_min
+    for label, hist in hists.items():
+        hist.GetYaxis().SetRangeUser(0.0, y_max + margin / k * y_range)
+        hist.GetYaxis().SetTitle("Normalized counts")
+        legend.AddEntry(hist, label, "L")
     legend.Draw()
 
-    hists["sig"].Write()
-    hists["bkg"].Write()
+    for hist in hists:
+        hist.Write()
     save_canvas(canv, c_name)
 
 
@@ -103,26 +108,28 @@ def plot_total_mass(hists_mass, c_name, hist_title):
     canv.SetGridx()
     canv.SetGridy()
 
-    mass_total = hists_mass["sig"].Clone(f"h_{c_name}")
-    #mass_total.Add(hists_mass["bkg"])
+    for i in sorted(hists_mass)[:1]:
+        mass_total = hists_mass[i].Clone(f"h_{c_name}")
+    for i in sorted(hists_mass)[2:]:
+        mass_total.Add(hists_mass[i])
     mass_total.SetTitle(hist_title)
     mass_total.Draw()
     mass_total.Write()
     save_canvas(canv, c_name)
 
 
-def plot_hists(settings, pt_ranges, trees):
+def plot_hists(settings, pt_ranges, trees, selections):
     """
     Plot histograms from trees.
     """
 
+    hist_colors = [kBlack, kRed+1, kBlue-3, kGreen+3]
     for var, settings_var in settings.items():
         hists = {}
         htype = "TH1F" if "#it{p}_{T}" in var else "TH2F"
-        for sig_or_bkg in ("sig", "bkg"):
-            hists[sig_or_bkg] = plot_tree_single(trees[sig_or_bkg], settings_var,
-                                                 f"Normalized {var}", sig_or_bkg,
-                                                 htype)
+        for ind, label, tree, selection in zip(trees.items(), selections):
+            hists[label] = plot_tree_single(tree, settings_var, f"Normalized {var}",
+                                            selection, hist_colors[ind], htype)
 
         if htype == "TH1F":
             plot_single(hists, settings_var[0])
@@ -132,12 +139,12 @@ def plot_hists(settings, pt_ranges, trees):
                 hist_title = f"{var} for {pt_ranges[i]}" \
                              f" #leq #it{{p}}_{{T}} < {pt_ranges[i + 1]}"
                 projs = {}
-                for sig_or_bkg in ("sig", "bkg"):
-                    ind = hists[sig_or_bkg].GetYaxis().FindBin(pt_ranges[i])
-                    ind2 = hists[sig_or_bkg].GetYaxis().FindBin(pt_ranges[i + 1] - 0.05)
-                    projs[sig_or_bkg] = hists[sig_or_bkg].ProjectionX(f"h_{sig_or_bkg}_{histname}",
+                for label in trees:
+                    ind = hists[label].GetYaxis().FindBin(pt_ranges[i])
+                    ind2 = hists[label].GetYaxis().FindBin(pt_ranges[i + 1] - 0.05)
+                    projs[label] = hists[label].ProjectionX(f"h_{label}_{histname}",
                                                                       ind, ind2)
-                    projs[sig_or_bkg].SetTitle(hist_title)
+                    projs[label].SetTitle(hist_title)
 
                 plot_single(projs, histname)
 
@@ -156,11 +163,12 @@ def main():
     gStyle.SetFrameLineWidth(2)
 
     parser = argparse.ArgumentParser(description="Arguments to pass")
-    parser.add_argument("sig_input_file", help="input signal tree AnalysisResults_tree.root file")
-    parser.add_argument("bkg_input_file", help="input bkg tree AnalysisResults_tree.root file")
-    parser.add_argument("output_file", help="output histograms file")
-
+    parser.add_argument("config_file", type=str, help="JSON config file")
     args = parser.parse_args()
+
+    with open(args.config_file, "r", encoding="utf-8") as config_f:
+        config_text = config_f.read()
+    config = json.loads(config_text)
 
     settings = { "mass": ("mass", "fM", [600, 2.18, 2.38]),
                  "decay length": ("decay_length", "fDecayLength", [100, 0.0, 0.1]),
@@ -182,15 +190,16 @@ def main():
                 }
     pt_ranges = [1, 2, 4, 6, 8, 12, 24]
 
-    infile_sig = TFile(args.sig_input_file)
-    infile_bkg = TFile(args.bkg_input_file)
-    outfile = TFile(args.output_file, "RECREATE") # pylint: disable=unused-variable
+    files = []
+    for file in config["input_files"]:
+        files.append(TFile(file))
+    outfile = TFile(config["output_file"], "RECREATE") # pylint: disable=unused-variable
 
     trees = {}
-    trees["sig"] = infile_sig.Get("DF_0/O2hfcandlcfull")
-    trees["bkg"] = infile_bkg.Get("DF0/O2hfcandlcfull")
+    for file, label in zip(files, config["labels"]):
+        trees[label] = file.Get("DF_0/O2hfcandlcfull")
 
-    plot_hists(settings, pt_ranges, trees)
+    plot_hists(settings, pt_ranges, trees, config["selections"])
 
 
 if __name__ == "__main__":
